1ce0961033e287b32de27972ad5dcc7bc8b0104f
Peter Hutchison <Peterhutch03@gmail.com>
2022-03-09 10:51:03 -0600

    Added increasing ball velocity

--- Breakout/GameScene.swift
+++ Breakout/GameScene.swift
@@ -34,7 +34,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     
     func kickBall() {
         ball.physicsBody?.isDynamic = true
-        ball.physicsBody?.applyImpulse(CGVector(dx: 3, dy: 5))
+        ball.physicsBody?.applyImpulse(CGVector(dx: Int.random(in: -5...5), dy: 5))
     }
     
     func updateLabels() {
@@ -185,26 +185,26 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     
     func didBegin(_ contact: SKPhysicsContact) {
         // ask each brick, "Is it you?"
-                for brick in bricks {
-                    if contact.bodyA.node == brick ||
-                        contact.bodyB.node == brick {
-                        score += 1
-                        updateLabels()
-                        if brick.color == .blue {
-                            brick.color = .orange   // blue bricks turn orange
-                        }
-                        else if brick.color == .orange {
-                            brick.color = .green    // orange bricks turn green
-                        }
-                        else {  // must be a green brick, which get removed
-                            brick.removeFromParent()
-                            removedBricks += 1
-                            if removedBricks == bricks.count {
-                                gameOver(winner: true)
-                            }
-                        }
+        for brick in bricks {
+            if contact.bodyA.node == brick ||
+                contact.bodyB.node == brick {
+                score += 1
+                updateLabels()
+                if brick.color == .blue {
+                    brick.color = .orange   // blue bricks turn orange
+                }
+                else if brick.color == .orange {
+                    brick.color = .green    // orange bricks turn green
+                }
+                else {  // must be a green brick, which get removed
+                    brick.removeFromParent()
+                    removedBricks += 1
+                    if removedBricks == bricks.count {
+                        gameOver(winner: true)
                     }
                 }
+            }
+        }
         
         if contact.bodyA.node?.name == "loseZone" ||
             contact.bodyB.node?.name == "loseZone" {
@@ -218,6 +218,9 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
                 gameOver(winner: false)
             }
         }
+        // increase ball velocity by 2%
+        ball.physicsBody!.velocity.dx = ball.physicsBody!.velocity.dx * CGFloat(1.02)
+        ball.physicsBody!.velocity.dy = ball.physicsBody!.velocity.dy * CGFloat(1.02)
     }
     
     func gameOver(winner: Bool) {
@@ -233,13 +236,13 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     }
     
     override func update(_ currentTime: TimeInterval) {
-            if abs(ball.physicsBody!.velocity.dx) < 100 {
-                // ball has stalled in x direction, so kick it randomly horizontally
-                ball.physicsBody?.applyImpulse(CGVector(dx: Int.random(in: -3...3), dy: 0))
-            }
-            if abs(ball.physicsBody!.velocity.dy) < 100 {
-                // ball has stalled in y direct, so kick it randomly vertically
-                ball.physicsBody?.applyImpulse(CGVector(dx: 0, dy: Int.random(in: -3...3)))
-            }
+        if abs(ball.physicsBody!.velocity.dx) < 100 {
+            // ball has stalled in x direction, so kick it randomly horizontally
+            ball.physicsBody?.applyImpulse(CGVector(dx: Int.random(in: -3...3), dy: 0))
         }
+        if abs(ball.physicsBody!.velocity.dy) < 100 {
+            // ball has stalled in y direct, so kick it randomly vertically
+            ball.physicsBody?.applyImpulse(CGVector(dx: 0, dy: Int.random(in: -3...3)))
+        }
+    }
 }
